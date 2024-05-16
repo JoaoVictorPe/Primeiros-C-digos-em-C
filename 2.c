12c4c42981ef953f8a8ce0e1b07bcc05b14570b0
João Victor <99892713+JoaoVictorPe@users.noreply.github.com>
2024-05-16 08:56:08 -0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+
+int main() {
+
+float nota1 = 0;
+float nota2 = 0;
+float nota3 = 0;
+float nota4 = 0;
+float total = 0;
+float media = 0;
+	
+	printf("Digite sua primeira nota: "); 
+	scanf("%f",  &nota1);
+	printf("Digite sua segunda nota: "); 
+	scanf("%f",  &nota2);
+	printf("Digite sua terceira nota: "); 
+	scanf("%f",  &nota3);
+	printf("Digite sua quarta nota: "); 
+	scanf("%f",  &nota4);
+	
+	total = nota1 + nota2 + nota3 + nota4;
+	media = total/4;
+	
+	printf("O total e %f:", total);
+	printf("\n");
+	printf(" A media e: %f", media);
+}
